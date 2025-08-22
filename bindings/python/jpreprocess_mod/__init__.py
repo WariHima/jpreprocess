e8df9f606663831d1987e194da09b381b43c8af8
WariHima <simamumin02@gmail.com>
2025-08-22 18:57:53 +0900

    update python bindings

--- bindings/python/jpreprocess_mod/__init__.py
+++ bindings/python/jpreprocess_mod/__init__.py
@@ -28,6 +28,17 @@ def jpreprocess(dictionary_version: str = f"v{JPREPROCESS_VERSION}", user_dictio
     - user_dictionary (str | None): Path to user dictionary. The extionsion must be ".csv" or ".bin".
     """
     dict_path = dictionary_path(dictionary_version)
+    latest_dict_path = dictionary_path("latest") 
+
     if not os.path.exists(dict_path):
-        download_dictionary(dictionary_version)
+        
+        if os.path.exists(latest_dict_path):
+            dict_path = latest_dict_path
+        
+        else:
+            try:
+                download_dictionary(dictionary_version)
+            except:
+                download_dictionary("latest")
+                
     return JPreprocess(dict_path, user_dictionary)
